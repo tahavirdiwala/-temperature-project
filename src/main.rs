b62f1e176dabb83d62524699235712c2622e735b
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-07-31 10:49:51 +0530

    learning ownership in rust

--- src/main.rs
+++ src/main.rs
@@ -34,3 +34,44 @@
 //     fibonacci_sequence(num-1) + fibonacci_sequence(num-2)
 // }
 
+//ownership 
+
+// fn main() {
+//    let x = 5;
+//    let y = x;
+
+//     println!("{}",  x==y)
+// }
+
+// fn main() {
+//     let s = String::from("hello"); 
+
+//     takes_ownership(s);          
+
+//     let x = 5;                   
+
+//     makes_copy(x);  
+    
+
+// } 
+// fn takes_ownership(some_string: String) { 
+//     println!("{some_string}");
+// }
+
+// fn makes_copy(some_integer: i32) { 
+//     println!("{some_integer}");
+// }
+
+fn main() {
+    let s1 = String::from("hello");
+
+    let (val, len) = get_len_val(s1);
+
+    println!("{}, {}", val, len)
+}
+
+fn get_len_val(val: String) -> (String, usize) {
+    let len = val.len();
+
+    (val, len)
+}
