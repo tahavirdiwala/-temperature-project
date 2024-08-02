e77963d6cee38e9ab25c3d48c158ef3fc732e324
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-08-02 21:45:03 +0530

    ..

--- src/main.rs
+++ src/main.rs
@@ -145,3 +145,22 @@
 
 //     println!("{}", slice)
 // }
+
+fn main() {
+    let s = String::from("hello world");
+
+    println!("{}", first_word(&s));
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+       if item == b' ' {
+        return &s[0..i];
+       }
+    }
+
+    &s[..]
+
+}
